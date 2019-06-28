ff2134a74125b223909cfb600e76e8cb7c8f48c7
Kaibaliev Chingiz <chikist_kg@mail.ru>
2019-06-28 19:14:16 +0600

    Wax

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+$("#slider1").owlCarousel({
+    paginationSpeed: 400,
+    nav:true,
+    navText: false,
+    loop:true,
+    items: 1,
+    itemsCustom: [
+        [0, 1],
+        [450, 2],
+        [600, 2],
+        [700, 2],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+    ],
+});
+
